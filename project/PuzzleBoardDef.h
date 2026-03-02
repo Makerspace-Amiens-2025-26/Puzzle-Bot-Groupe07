6f8ccb7f38d4acfa7e75983844f885363625ce18
mathieulardereau <mathieu.lardereau@etu.unilasalle.fr>
2026-03-02 10:54:11 +0100

    Ajout PuzzleBotBoarddef
    Puzzle Bot Pin def for code

---
+++ project/PuzzleBoardDef.h
@@ -0,0 +1,20 @@
+
+namespace PIN{
+    namespace Steppers{
+        constexpr int
+            STEP1 = 2,
+            STEP2 = 3,
+            EN = 8, 
+            DIR1 = 5,
+            DIR2 = 6;
+    } 
+    namespace Capteur{
+        constexpr int 
+            Capteur1 = 9,
+            Capteur2 = 10;
+    }
+    namespace Servo{
+        constexpr int 
+            Servo1 = 11;
+    }
+}
